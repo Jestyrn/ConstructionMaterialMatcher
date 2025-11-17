cfcb00a55570c58131f7e604bc318cb38be2bdfb
Jestyrn <mr9rey@yandex.ru>
2025-11-17 12:26:02 +0400

    Create repo

---
+++ ConstructionMaterialMatcher/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ConstructionMaterialMatcher.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ConstructionMaterialMatcher/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ConstructionMaterialMatcher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
